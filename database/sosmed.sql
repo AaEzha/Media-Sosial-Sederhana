4f7e133dc0925cfe9942a699e01638b7b07bb95a
Reza 'Aa' Nurfachmi <aaezha@gmail.com>
2019-07-02 02:46:39 +0700

    Add files via upload

--- database/sosmed.sql
+++ database/sosmed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 01, 2019 at 09:41 PM
+-- Generation Time: Jul 01, 2019 at 09:45 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.1.26
 
@@ -37,19 +37,6 @@ CREATE TABLE `postingan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `postingan`
---
-
-INSERT INTO `postingan` (`id`, `parent`, `nama`, `pesan`, `waktu`) VALUES
-(1, 0, 'Aa Ezha', 'Assalamu\'alaikum warahmatullahi wabarakatuh.', '2019-07-01 20:50:24'),
-(2, 0, 'Ibrahim', 'Selamat belajar PHP di channel youtube Aa Ezha.', '2019-07-01 20:51:35'),
-(3, 0, 'Mikael', 'Selamat pagi, semuanya! Bagaimana kabar kalian? Saya harap semuanya dalam keadaan sehat wal afiat.', '2019-07-01 21:00:37'),
-(4, 1, 'Andi', 'Wa\'alaikumsalam warahmatullahi wabarakatuh.', '2019-07-01 21:13:16'),
-(5, 2, 'Andini', 'Semoga saya bisa memahami materi-materi pada channel Aa Ezha.', '2019-07-01 21:15:08'),
-(6, 3, 'Lorensius', 'Puji Tuhan, kabar saya hari ini sehat dan gembira sekali.', '2019-07-01 21:19:00'),
-(7, 3, 'Firda', 'Selamat pagi juga, Mikael. Saya juga di sini alhamdulilah sehat wal afiat. Semoga kita selalu sehat, yah!', '2019-07-01 21:19:39');
-
---
 -- Indexes for dumped tables
 --
 
@@ -67,7 +54,7 @@ ALTER TABLE `postingan`
 -- AUTO_INCREMENT for table `postingan`
 --
 ALTER TABLE `postingan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
